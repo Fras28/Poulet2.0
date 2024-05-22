d5a59adf7e3999ec1c572907ed71013fda2ef423
Fras28 <fselvarolo28@gmail.com>
2024-05-22 13:16:06 -0300

    edter

--- src/Components/Categorias/Editer.jsx
+++ src/Components/Categorias/Editer.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CardsEdite } from "../Cards/CardsEdit.jsx";
-import { asyncAllProducts, asyncArticulos, asyncSubCategoria } from "../redux/slice.jsx";
+import { asyncAllProducts } from "../redux/slice.jsx";
 
 export const Editer = () => {
   const dispatch = useDispatch();
-  const { subCategorias, allProduct } = useSelector((state) => state.alldata);
+  const { allProduct } = useSelector((state) => state.alldata);
 
   useEffect(() => {
     dispatch(asyncAllProducts());
@@ -13,17 +13,27 @@ export const Editer = () => {
 
   // Estado para el valor de búsqueda
   const [searchValue, setSearchValue] = useState("");
-  const [filteredProducts, setFilteredProducts] = useState(allProduct);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  // Efecto secundario para actualizar filteredProducts cuando allProduct cambie
+  useEffect(() => {
+    setFilteredProducts(allProduct);
+  }, [allProduct]);
 
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setSearchValue(value);
 
-    // Filtrar los productos basados en el valor de búsqueda
-    const filtered = allProduct.filter(product =>
-      product.attributes.name.toLowerCase().includes(value.toLowerCase())
-    );
-    setFilteredProducts(filtered);
+    // Si el valor de búsqueda está vacío, enviar directamente allProduct
+    if (value === "") {
+      setFilteredProducts(allProduct);
+    } else {
+      // Filtrar los productos basados en el valor de búsqueda
+      const filtered = allProduct.filter(product =>
+        product.attributes.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
   };
 
   return (
